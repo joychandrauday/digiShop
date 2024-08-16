a7463981a435b0748540448a4cdaae3f9e17d351
joychandrauday <joychandraud@gmail.com>
2024-08-16 18:38:15 +0600

    making sure everything is up to date

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 8000
 
 // middleware
 const corsOptions = {
-  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  origin: ['http://localhost:5173', 'http://localhost:5174','https://digish0p.web.app'],
   credentials: true,
   optionSuccessStatus: 200,
 }
